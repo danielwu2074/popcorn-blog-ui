2808edfc734fda8ccf0839959a9318d6987b4c08
wuwh <wuwh@yunjiacloud.com>
2021-08-17 16:45:36 +0800

    init

---
+++ vue_popcorn_admin/config/dev.env.js
@@ -0,0 +1,10 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+
+  //开发环境
+  ADMIN_API: '"http://localhost:8607/mogu-admin"'
+})
